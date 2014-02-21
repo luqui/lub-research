0ff1d9d4f802532ffd2899b4c4c7ee674df8a775
Luke Palmer <luqui@google.com>
2014-02-21 11:54:22 -0800

    Added a benchmark

---
+++ benchmark.hs
@@ -0,0 +1,10 @@
+import qualified LazyNaturals as LN
+import qualified Criterion.Main as C
+
+
+benchPlus3 :: C.Benchmark
+benchPlus3 = C.bench "plus3" $ 
+  C.nf (\n -> LN.isAtLeast (2*n) (LN.plus3 (LN.atLeast n) (LN.atLeast n))) 100
+
+
+main = C.defaultMain [benchPlus3]
